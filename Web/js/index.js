4329016c4eb4783ce73b25dd62961f0230069bea
Bozeyy <spereaux2@gmail.com>
2023-06-20 18:21:39 +0200

    ajout de la méthode pour envoyer une réservation

--- Web/js/index.js
+++ Web/js/index.js
@@ -1,28 +1,28 @@
-import {fetchIncidents} from "./Incidents.js";
+import { fetchIncidents } from "./Incidents.js";
 
 // Créer une carte avec Leaflet et la centrer sur les coordonnées spécifiées
 var map = L.map("map").setView([48.6921, 6.1844], 13);
 
 // Ajouter une couche de tuiles OpenStreetMap à la carte
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
 export const defaultIcon = L.Icon.extend({
-    options: {
-        iconSize: [40, 40],
-        //iconAnchor: [22, 94],
-        //popupAnchor: [-3, -76], Déplace les logos quand on unzoom ?
-    },
+  options: {
+    iconSize: [40, 40],
+    //iconAnchor: [22, 94],
+    //popupAnchor: [-3, -76], Déplace les logos quand on unzoom ?
+  },
 });
 const etablissementIcon = new defaultIcon({
-    iconUrl: "../logo/EtablissementSup.png",
+  iconUrl: "../logo/EtablissementSup.png",
 });
-const restaurantIcon = new defaultIcon({iconUrl: "../logo/Restaurant.png"});
+const restaurantIcon = new defaultIcon({ iconUrl: "../logo/Restaurant.png" });
 export const stationIcon = new defaultIcon({
-    iconUrl: "../logo/StationVelo.png",
+  iconUrl: "../logo/StationVelo.png",
 });
 
 /**
@@ -30,21 +30,21 @@ export const stationIcon = new defaultIcon({
  * @param {Array} restaurants - Un tableau d'objets représentant les restaurants.
  */
 export function addRestaurantMarkers(restaurants) {
-    restaurants.forEach(function (restaurant) {
-        // Créer un marqueur pour chaque restaurant et l'ajouter à la carte
-        var marker = L.marker([restaurant.latitude, restaurant.longitude], {
-            icon: restaurantIcon,
-        }).addTo(map);
+  restaurants.forEach(function (restaurant) {
+    // Créer un marqueur pour chaque restaurant et l'ajouter à la carte
+    var marker = L.marker([restaurant.latitude, restaurant.longitude], {
+      icon: restaurantIcon,
+    }).addTo(map);
 
-        // Associer une info-bulle au marqueur contenant le nom du restaurant
-        marker.bindPopup(restaurant.nom);
+    // Associer une info-bulle au marqueur contenant le nom du restaurant
+    marker.bindPopup(restaurant.nom);
 
-        // Ajouter un événement de clic pour chaque marqueur
-        marker.on("click", function () {
-            // Afficher le formulaire d'inscription
-            showInscriptionForm(restaurant);
-        });
+    // Ajouter un événement de clic pour chaque marqueur
+    marker.on("click", function () {
+      // Afficher le formulaire d'inscription
+      showInscriptionForm(restaurant);
     });
+  });
 }
 
 /**
@@ -52,123 +52,163 @@ export function addRestaurantMarkers(restaurants) {
  * @param {Object} restaurant - L'objet représentant le restaurant.
  */
 function showInscriptionForm(restaurant) {
-    let formElement = document.querySelector(".reservation");
-    formElement.style.display = "flex";
+  let formElement = document.querySelector(".reservation");
+  formElement.style.display = "flex";
 }
 
 // Ajouter un gestionnaire d'événement au bouton de fermeture du formulaire
 let closebtn = document.querySelector("#close");
 closebtn.addEventListener("click", function (event) {
-    let formElement = document.querySelector(".reservation");
-    formElement.style.display = "none";
+  let formElement = document.querySelector(".reservation");
+  formElement.style.display = "none";
 });
 
 // Ajouter les marqueurs des restaurants de test à la carte
 // addRestaurantMarkers(resTest);
 
 // URL de l'API pour récupérer les restaurants
-let URL_API = "http://10.11.58.65:9000/restaurant/getTables";
+let URL_API = "http://192.168.1.25:9000/restaurant/getTables";
 
 // Effectuer une requête vers l'API pour récupérer les restaurants
-fetch(URL_API, {method: "GET"})
-    .then((response) => response.json())
-    .then((data) => {
-        // Ajouter les marqueurs des restaurants récupérés à la carte
-        addRestaurantMarkers(data);
-    })
-    .catch((error) => console.log(error));
+fetch(URL_API, { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => {
+    // Ajouter les marqueurs des restaurants récupérés à la carte
+    addRestaurantMarkers(data);
+  })
+  .catch((error) => console.log(error));
 
 // Effectuer une requête vers l'API pour récupérer les etablissements
 
-fetch("http://10.11.58.65:9000/recupererListEtablissement", {method: "GET"})
-    .then((response) => response.json())
-    .then((data) => {
-        addEtablissementMarker(data);
-    });
+fetch("http://192.168.1.25:9000/recupererListEtablissement", { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => {
+    addEtablissementMarker(data);
+  });
 
 export function addEtablissementMarker(etablissement) {
-    etablissement.forEach(function (etablissement) {
-        // Créer un marqueur pour chaque restaurant et l'ajouter à la carte
-        if (!etablissement.fields.coordonnees) return;
-        var marker = L.marker(
-            [
-                etablissement.fields.coordonnees[0],
-                etablissement.fields.coordonnees[1],
-            ],
-            {icon: etablissementIcon}
-        ).addTo(map);
+  etablissement.forEach(function (etablissement) {
+    // Créer un marqueur pour chaque restaurant et l'ajouter à la carte
+    if (!etablissement.fields.coordonnees) return;
+    var marker = L.marker(
+      [
+        etablissement.fields.coordonnees[0],
+        etablissement.fields.coordonnees[1],
+      ],
+      { icon: etablissementIcon }
+    ).addTo(map);
 
-        // Associer une info-bulle au marqueur contenant le nom du restaurant
-        marker.bindPopup(etablissement.fields.url);
-
-        // Ajouter un événement de clic pour chaque marqueur
-        marker.on("click", function () {
-            // Afficher le formulaire d'inscription
-        });
-    });
-}
-
-function addStationMarker(station) {
-    var marker = L.marker([station.lat, station.lon], {
-        icon: stationIcon,
-    }).addTo(map);
     // Associer une info-bulle au marqueur contenant le nom du restaurant
-    marker.bindPopup(
-        "Adresse de la station: " +
-        station.address +
-        " nombre de vélos disponible sur la station: " +
-        station.veloDispo +
-        " nombre de place de parkings libre sur la station: " +
-        station.placesDispo
-    );
+    marker.bindPopup(etablissement.fields.url);
 
     // Ajouter un événement de clic pour chaque marqueur
     marker.on("click", function () {
-        // Afficher le formulaire d'inscription
+      // Afficher le formulaire d'inscription
     });
+  });
+}
+
+function addStationMarker(station) {
+  var marker = L.marker([station.lat, station.lon], {
+    icon: stationIcon,
+  }).addTo(map);
+  // Associer une info-bulle au marqueur contenant le nom du restaurant
+  marker.bindPopup(
+    "Adresse de la station: " +
+      station.address +
+      " nombre de vélos disponible sur la station: " +
+      station.veloDispo +
+      " nombre de place de parkings libre sur la station: " +
+      station.placesDispo
+  );
+
+  // Ajouter un événement de clic pour chaque marqueur
+  marker.on("click", function () {
+    // Afficher le formulaire d'inscription
+  });
 }
 
 // Partie pour les stations de vélo
 const bike = async () => {
-    // Effectuer une requête vers l'API ouverte transport pour récupérer les stations de vélo
-    const [infoStations, statusStations] = await Promise.all([
-        // Information sur les stations
-        fetch(
-            "https://transport.data.gouv.fr/gbfs/nancy/station_information.json",
-            {method: "GET"}
-        ),
-        // Status en temps réel des stations
-        fetch("https://transport.data.gouv.fr/gbfs/nancy/station_status.json", {
-            method: "GET",
-        }),
-    ]);
-    // Extraire les données JSON des réponses
-    const infoStationJson = await infoStations.json();
-    const statusStationsJson = await statusStations.json();
+  // Effectuer une requête vers l'API ouverte transport pour récupérer les stations de vélo
+  const [infoStations, statusStations] = await Promise.all([
+    // Information sur les stations
+    fetch(
+      "https://transport.data.gouv.fr/gbfs/nancy/station_information.json",
+      { method: "GET" }
+    ),
+    // Status en temps réel des stations
+    fetch("https://transport.data.gouv.fr/gbfs/nancy/station_status.json", {
+      method: "GET",
+    }),
+  ]);
+  // Extraire les données JSON des réponses
+  const infoStationJson = await infoStations.json();
+  const statusStationsJson = await statusStations.json();
 
-    // Ajouter les marqueurs des stations récupérées à la carte
-    console.log(infoStationJson);
-    console.log(statusStationsJson);
-    infoStationJson.data.stations.forEach((station) => {
-        const status = statusStationsJson.data.stations.find(
-            (s) => s.station_id === station.station_id
-        );
-        if (status) {
-            station.veloDispo = status.num_bikes_available;
-            station.placesDispo = status.num_docks_available;
-            addStationMarker(station);
-        }
-    });
+  // Ajouter les marqueurs des stations récupérées à la carte
+  console.log(infoStationJson);
+  console.log(statusStationsJson);
+  infoStationJson.data.stations.forEach((station) => {
+    const status = statusStationsJson.data.stations.find(
+      (s) => s.station_id === station.station_id
+    );
+    if (status) {
+      station.veloDispo = status.num_bikes_available;
+      station.placesDispo = status.num_docks_available;
+      addStationMarker(station);
+    }
+  });
 };
 
 bike();
 
 export const addMarker = (nord, est, icon, descr) => {
-    var marker = L.marker([nord, est], {
-        icon: icon,
-    }).addTo(map);
-    // Associer une info-bulle au marqueur contenant le nom du restaurant
-    marker.bindPopup(descr);
+  var marker = L.marker([nord, est], {
+    icon: icon,
+  }).addTo(map);
+  // Associer une info-bulle au marqueur contenant le nom du restaurant
+  marker.bindPopup(descr);
 };
 
 fetchIncidents();
+
+function envoyerReservation() {
+  let prenom = document.getElementById("name").value;
+  let nom = document.getElementById("surname").value;
+  let nombrePersonnes = document.getElementById("nbper").value;
+  let idTable = document.getElementById("numtable").value;
+  let date = document.getElementById("date").value;
+  let heure = document.getElementById("time").value;
+
+  // Créer un objet contenant les données de réservation
+  const reservationData = {
+    nom: nom,
+    prenom: prenom,
+    nbPersonnes: nombrePersonnes,
+    numTable: idTable,
+    date: date,
+    date: date+"/"+heure+":00",
+  };
+
+  // Envoyer les données de réservation à l'API
+  fetch("http://192.168.1.25:9000/restaurant/reserverTable", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(reservationData),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Réservation effectuée avec succès !");
+        // Effectuer des actions supplémentaires après une réservation réussie
+      } else {
+        throw new Error("Erreur lors de la réservation : " + response.status);
+      }
+    })
+    .catch((error) => {
+      console.log("Erreur lors de la réservation : " + error.message);
+      // Gérer les erreurs de réservation
+    });
+}
--- out/production/SAE_projet-d-application-repartie/js/index.js
+++ out/production/SAE_projet-d-application-repartie/js/index.js
@@ -46,38 +46,100 @@ closebtn.addEventListener("click", function (event) {
   formElement.style.display = "none";
 });
 
-// Données de test pour les restaurants
-let resTest = [
-  {
-    nom: "Restaurant A",
-    longitude: 6.1784,
-    latitude: 48.6921,
-  },
-  {
-    nom: "Restaurant B",
-    longitude: 6.183,
-    latitude: 48.6929,
-  },
-  {
-    nom: "Restaurant C",
-    longitude: 6.1694,
-    latitude: 48.6866,
-  },
-];
-
-// Ajouter les marqueurs des restaurants de test à la carte
-addRestaurantMarkers(resTest);
 
 // URL de l'API pour récupérer les restaurants
-let URL_API = "http://10.11.17.202:9000/restaurants/getTables";
+let URL_API = "http://10.11.58.65:9000/restaurant/getTables";
 
 // Effectuer une requête vers l'API pour récupérer les restaurants
-fetch(URL_API, { method: "GET" })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("Résultat de la requête :");
-    console.log(data);
-    // Ajouter les marqueurs des restaurants récupérés à la carte
-    addRestaurantMarkers(data);
-  })
-  .catch((error) => console.log(error));
+fetch(URL_API, {method: "GET"})
+    .then((response) => response.json())
+    .then((data) => {
+        // Ajouter les marqueurs des restaurants récupérés à la carte
+        addRestaurantMarkers(data);
+    })
+    .catch((error) => console.log(error));
+
+// Effectuer une requête vers l'API pour récupérer les etablissements
+
+fetch("http://10.11.58.65:9000/recupererListEtablissement", {method: "GET"})
+    .then((response) => response.json())
+    .then((data) => {
+        addEtablissementMarker(data);
+    });
+
+export function addEtablissementMarker(etablissement) {
+    etablissement.forEach(function (etablissement) {
+        // Créer un marqueur pour chaque restaurant et l'ajouter à la carte
+        if (!etablissement.fields.coordonnees) return;
+        var marker = L.marker(
+            [
+                etablissement.fields.coordonnees[0],
+                etablissement.fields.coordonnees[1],
+            ],
+            {icon: etablissementIcon}
+        ).addTo(map);
+
+        // Associer une info-bulle au marqueur contenant le nom du restaurant
+        marker.bindPopup(etablissement.fields.url);
+
+        // Ajouter un événement de clic pour chaque marqueur
+        marker.on("click", function () {
+            // Afficher le formulaire d'inscription
+        });
+    });
+}
+
+function addStationMarker(station) {
+    var marker = L.marker([station.lat, station.lon], {
+        icon: stationIcon,
+    }).addTo(map);
+    // Associer une info-bulle au marqueur contenant le nom du restaurant
+    marker.bindPopup(
+        "Adresse de la station: " +
+        station.address +
+        " nombre de vélos disponible sur la station: " +
+        station.veloDispo +
+        " nombre de place de parkings libre sur la station: " +
+        station.placesDispo
+    );
+
+    // Ajouter un événement de clic pour chaque marqueur
+    marker.on("click", function () {
+        // Afficher le formulaire d'inscription
+    });
+}
+
+// Partie pour les stations de vélo
+const bike = async () => {
+    // Effectuer une requête vers l'API ouverte transport pour récupérer les stations de vélo
+    const [infoStations, statusStations] = await Promise.all([
+        // Information sur les stations
+        fetch(
+            "https://transport.data.gouv.fr/gbfs/nancy/station_information.json",
+            {method: "GET"}
+        ),
+        // Status en temps réel des stations
+        fetch("https://transport.data.gouv.fr/gbfs/nancy/station_status.json", {
+            method: "GET",
+        }),
+    ]);
+    // Extraire les données JSON des réponses
+    const infoStationJson = await infoStations.json();
+    const statusStationsJson = await statusStations.json();
+
+    // Ajouter les marqueurs des stations récupérées à la carte
+    console.log(infoStationJson);
+    console.log(statusStationsJson);
+    infoStationJson.data.stations.forEach((station) => {
+        const status = statusStationsJson.data.stations.find(
+            (s) => s.station_id === station.station_id
+        );
+        if (status) {
+            station.veloDispo = status.num_bikes_available;
+            station.placesDispo = status.num_docks_available;
+            addStationMarker(station);
+        }
+    });
+};
+
+bike();
